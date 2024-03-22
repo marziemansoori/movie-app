1ba2e9789984b937e0af9a38ddc60090229b3dc6
marziemansoori <marzie.masnoori@gmail.com>
2024-03-22 13:21:44 +0000

    Add user rating api, add service discovery micro service

---
+++ movie-catalog-service/src/main/java/org/example/moviecatalogservice/entitites/Movie.java
@@ -0,0 +1,29 @@
+package org.example.movieinfoservice.entities;
+
+public class Movie {
+
+    private String id;
+
+    private String name;
+
+    public Movie(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
